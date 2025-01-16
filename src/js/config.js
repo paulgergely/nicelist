d0f360c82ff0c05b0031f58e1356ce5f31e8a8a0
paulgergely <paulgergely@gmail.com>
2025-01-16 12:26:05 +0100

    Revert folder structure to original state

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-
-export const supabase = createClient(
-    'https://cqkosaadwkqghieplsio.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxa29zYWFkd2txZ2hpZXBsc2lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NjE4ODgsImV4cCI6MjA1MjQzNzg4OH0.i_6K2Ehzmj9NKmKv-zOFkcwH7bF5tG-Tw4naOiPfOfo'
-); 
